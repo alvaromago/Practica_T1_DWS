c3699727994b62c8c77e770b19be2fc78223f7a3
alvaromago <aalvaromago@gmail.com>
2023-11-06 20:22:48 +0100

    -

--- carrito.php
+++ carrito.php
@@ -1,18 +1,27 @@
 <?php
 session_start();
 
+$precios = array(
+    "Camiseta negra manga corta" => 15.00,
+    "Vaquero azul oscuro" => 30.00,
+    "Zapatillas de piel color blanca" => 60.00
+);
+
 // Inicializa la sesión si no está configurada
 if (!isset($_SESSION['carrito'])) {
     $_SESSION['carrito'] = array();
 }
+
 
 // Agregar producto al carrito si se envía el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $producto = $_POST['producto'];
     $cantidad = (int)$_POST['cantidad'];
 
+    $precio = $precios[$producto];
+
     if ($cantidad > 0) {
-        $_SESSION['carrito'][] = array('producto' => $producto, 'cantidad' => $cantidad);
+        $_SESSION['carrito'][] = array('producto' => $producto, 'cantidad' => $cantidad, 'precio' => $precio);
     }
 }
 
@@ -31,9 +40,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <ul>
         <?php
         if (!empty($_SESSION['carrito'])) {
+            $total = 0;
             foreach ($_SESSION['carrito'] as $item) {
-                echo "<p>{$item['producto']} - Cantidad: {$item['cantidad']}</p>";
+                $subtotal = $item['cantidad'] * $item['precio'];
+                $total += $subtotal;
+                echo "<p>{$item['producto']} - Cantidad: {$item['cantidad']} - Precio: $" . number_format($item['precio'], 2) . " - Subtotal: $" . number_format($subtotal, 2) . "</p>";
             }
+            echo "<p>Total: $" . number_format($total, 2) . "</p>";
         } else {
             echo "<p>El carrito está vacío.</p>";
         }
--- pedidos.php
+++ pedidos.php
@@ -1,35 +1,48 @@
 <?php
 session_start();
 
-// Inicializa la sesión si no está configurada
-if (!isset($_SESSION['carrito'])) {
-    $_SESSION['carrito'] = array();
-}
+// Comprueba si se ha presionado el botón "Borrar historial"
+if (isset($_POST['borrar_historial'])) {
+    // Elimina las cookies relacionadas con el historial de pedidos, el número de pedidos y la fecha del último pedido
+    setcookie('numero_pedidos', '', time() - 3600, '/');
+    setcookie('fecha_ultimo_pedido', '', time() - 3600, '/');
+    setcookie('historial_pedidos', '', time() - 3600, '/');
+} else {
+    // Procesa el pedido y actualiza el historial
+    if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
+        // Obtén el número total de pedidos y la fecha del último pedido desde las cookies
+        $numero_pedidos = isset($_COOKIE['numero_pedidos']) ? $_COOKIE['numero_pedidos'] : 0;
+        $fecha_ultimo_pedido = isset($_COOKIE['fecha_ultimo_pedido']) ? $_COOKIE['fecha_ultimo_pedido'] : 'N/A';
 
-// Agregar producto al carrito si se envía el formulario
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $producto = $_POST['producto'];
-    $cantidad = (int)$_POST['cantidad'];
+        // Incrementa el número de pedidos
+        $numero_pedidos++;
 
-    if ($cantidad > 0) {
-        $_SESSION['carrito'][] = array('producto' => $producto, 'cantidad' => $cantidad);
+        // Obtiene la fecha y hora actual
+        $fecha_actual = date('d-m-y H:i:s');
+
+        // Actualiza la fecha del último pedido
+        $fecha_ultimo_pedido = $fecha_actual;
+
+        // Crea el historial de pedidos
+        $historial_pedidos = [];
+        if (isset($_COOKIE['historial_pedidos'])) {
+            $historial_pedidos = unserialize($_COOKIE['historial_pedidos']);
+        }
+
+        // Agrega el pedido actual al historial
+        $historial_pedidos[] = ['productos' => $_SESSION['carrito'], 'fecha' => $fecha_actual];
+
+        // Almacena la información actualizada en las cookies
+        setcookie('numero_pedidos', $numero_pedidos, time() + 1800, '/');
+        setcookie('fecha_ultimo_pedido', $fecha_ultimo_pedido, time() + 1800, '/');
+        setcookie('historial_pedidos', serialize($historial_pedidos), time() + 1800, '/');
+
+        // Borra la sesión del carrito
+        $_SESSION['carrito'] = array();
     }
-}
-
-// Procesar pedido y actualizar el historial
-if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
-    if (isset($_COOKIE['numero_pedidos'])) {
-        $numero_pedidos = $_COOKIE['numero_pedidos'] + 1;
-    } else {
-        $numero_pedidos = 1;
-    }
-    $fecha_ultimo_pedido = date('Y-m-d H:i:s');
-    setcookie('numero_pedidos', $numero_pedidos, time() + 3600, '/');
-    setcookie('fecha_ultimo_pedido', $fecha_ultimo_pedido, time() + 3600, '/');
-    // Borra la sesión del carrito
-    $_SESSION['carrito'] = array();
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="es">
@@ -42,20 +55,23 @@ if (isset($_SESSION['carrito']) && !empty($_SESSION['carrito'])) {
 
 <body>
     <h1>Historial de Pedidos</h1>
+    <?php
+    if (isset($numero_pedidos)) {
+        echo 'Número total de pedidos: ' . $numero_pedidos;
+        echo '<br>';
+        echo 'Fecha del último pedido: ' . $fecha_ultimo_pedido;
+    } ?>
     <ul>
         <?php
-        if (!empty($_SESSION['carrito'])) {
-            foreach ($_SESSION['carrito'] as $item) {
-                echo "<li>{$item['producto']} - Cantidad: {$item['cantidad']}</li>";
-            }
+        if (!empty($historial_pedidos)) {
         } else {
-            echo "<li>El carrito está vacío.</li>";
+            echo "<li>El historial está vacío.</li>";
         }
         ?>
     </ul>
-    <a href="inicio.php">Seguir comprando</a>
+    <a href="inicio.php">Volver al inicio</a>
     <form action="pedidos.php" method="post">
-        <input type="submit" value="Borrar historial">
+        <input type="submit" name="borrar_historial" value="Borrar historial">
     </form>
 </body>
 
